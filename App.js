d4babfdb50d001026210d34af4a8b8141f82899d
leonardoLimaDeSouzaLopes <91790685+leonardoLimaDeSouzaLopes@users.noreply.github.com>
2024-07-30 08:42:38 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,88 @@
+import { StatusBar } from 'expo-status-bar';
+import { Image, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>  
+      <StatusBar style="auto" />
+      
+      <View style={styles.titulo}>
+        <Image source={require('./assets/titulo.png')}/>
+      </View>
+
+      <View style={styles.placar}>
+        <View style={styles.placarTitulo}> 
+          <Text>Placar</Text>
+        </View>
+        <View style={styles.placarPontos}>
+          <View style={styles.pontos}>
+            <Text>0</Text>
+          </View>
+          <View style={styles.pontos}>
+            <Text>0</Text>
+          </View>
+        </View> 
+      </View>
+
+      <View style={styles.jogo}>
+        <View style={styles.jogador}>
+          <Image source={require('./assets/caixa.png')}/>
+        </View>
+        <View style={styles.vs}>
+          <Image source={require('./assets/vs.png')}/>
+        </View>
+        <View style={styles.jogador}>
+          <Image source={require('./assets/caixa.png')}/>
+        </View>
+      </View>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  titulo: {
+    flex: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  placar: {
+    flex: 3,
+    width: "100%",
+  },
+  placarTitulo: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  placarPontos: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  pontos: {
+    flex:1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  jogo: {
+    flex: 2,
+    width: "100%",
+    flexDirection: "row",
+  },
+  jogador: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  vs: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
