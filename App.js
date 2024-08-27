fde92f7ae1b67606161aeedc34a5622d845d9ee2
leonardoLimaDeSouzaLopes <91790685+leonardoLimaDeSouzaLopes@users.noreply.github.com>
2024-08-27 08:39:59 -0300

    Terminei o Projeto!
    Adicionei a contabilização de pontos e a opção de reiniciar o jogo.

--- App.js
+++ App.js
@@ -7,76 +7,96 @@ export default function App() {
 
   const [visivel, setVisivel] = useState(false);
 
-  const [jogadorImagem, setJogadorImagem] = useState(require('./assets/caixa.png'));
+  const [pontosJogador, setPontosJogador] = useState(0);
 
-  const [cpuImagem, setCpuImagem] = useState(require('./assets/caixa.png'));
+  const [pontosCpu, setPontosCpu] = useState(0);
 
-  const [figura, setFigura] = useState([
+  const [imagemPadrao, setImagemPadrao] = useState(require('./assets/caixa.png'))
+
+  const [jogadorImagem, setJogadorImagem] = useState(imagemPadrao);
+
+  const [cpuImagem, setCpuImagem] = useState(imagemPadrao);
+
+  const [figuras, setFiguras] = useState([
     {
       nome: "Pedra",
       imagem: require('./assets/pedra.png'),
       vantagens: [1, 2, 3, 4],
-      desvantagens: [5, 6, 7, 8],
     },
     {
       nome: "Fogo",
       imagem: require('./assets/fogo.png'),
       vantagens: [2, 3, 4, 5],
-      desvantagens: [0, 6, 7, 8],
     },
     {
       nome: "Tesoura",
       imagem: require('./assets/tesoura.png'),
       vantagens: [3, 4, 5, 6],
-      desvantagens: [0, 1, 7, 8],
     },
     {
       nome: "Pessoa",
       imagem: require('./assets/pessoa.png'),
       vantagens: [4, 5, 6, 7],
-      desvantagens: [0, 1, 2, 8],
     },
     {
       nome: "Esponja",
       imagem: require('./assets/esponja.png'),
       vantagens: [5, 6, 7, 8],
-      desvantagens: [0, 1, 2, 3],
     },
     {
       nome: "Papel",
       imagem: require('./assets/papel.png'),
       vantagens: [6, 7, 8, 0],
-      desvantagens: [1, 2, 3, 4],
     },
     {
       nome: "Ar",
       imagem: require('./assets/ar.png'),
       vantagens: [7, 8, 0, 1],
-      desvantagens: [2, 3, 4, 5],
     },
     {
       nome: "Água",
       imagem: require('./assets/agua.png'),
       vantagens: [8, 0, 1, 2],
-      desvantagens: [3, 4, 5, 6],
     },
     {
       nome: "Arma",
       imagem: require('./assets/arma.png'),
       vantagens: [0, 1, 2, 3],
-      desvantagens: [4, 5, 6, 7],
     },
 
   ])
 
   const jogar = (i) => {
 
-    setJogadorImagem(figura[i].imagem);
+    setJogadorImagem(figuras[i].imagem);
 
-    var numRandom = (Math.random() * 9) | 0;
+    var escolhaCpu = (Math.random() * 9) | 0;
 
-    setCpuImagem(figura[numRandom].imagem);
+    setCpuImagem(figuras[escolhaCpu].imagem);
 
+    figuras[i].vantagens.forEach(figura => {
+      
+      if(figura == escolhaCpu) {
+        setPontosJogador(pontosJogador + 1);
+      }
+
+    });
+
+    figuras[escolhaCpu].vantagens.forEach(figura => {
+      
+      if(figura == i) {
+        setPontosCpu(pontosCpu + 1);
+      }
+      
+    });
+
+  };
+
+  const novaPartida = () => {
+    setPontosJogador(0);
+    setPontosCpu(0);
+    setJogadorImagem(imagemPadrao)
+    setCpuImagem(imagemPadrao)
   }
 
   return (
@@ -102,10 +122,10 @@ export default function App() {
           </View>
           <View style={styles.placarPontos}>
             <View style={styles.pontos}>
-              <Text style={styles.textoPontos}>0</Text>
+              <Text style={styles.textoPontos}>{pontosJogador}</Text>
             </View>
             <View style={styles.pontos}>
-              <Text style={styles.textoPontos}>0</Text>
+              <Text style={styles.textoPontos}>{pontosCpu}</Text>
             </View>
           </View>
         </View>
@@ -126,8 +146,9 @@ export default function App() {
         </View>
 
         <View style={styles.novoJogo}>
-          <Pressable>
+          <Pressable onPress={() => novaPartida()}>
             <Image source={require('./assets/nova-partida.png')} />
+
           </Pressable>
         </View>
 
@@ -136,35 +157,35 @@ export default function App() {
           <ScrollView>
             <View style={styles.fileira}>
               <Pressable style={styles.item} onPress={() => jogar(0)}>
-                <Image style={styles.itemImagem} source={figura[0].imagem} />
+                <Image style={styles.itemImagem} source={figuras[0].imagem} />
               </Pressable>
               <Pressable style={styles.item} onPress={() => jogar(1)}>
-                <Image style={styles.itemImagem} source={figura[1].imagem} />
+                <Image style={styles.itemImagem} source={figuras[1].imagem} />
               </Pressable>
               <Pressable style={styles.item} onPress={() => jogar(2)}>
-                <Image style={styles.itemImagem} source={figura[2].imagem} />
+                <Image style={styles.itemImagem} source={figuras[2].imagem} />
               </Pressable>
             </View>
             <View style={styles.fileira}>
               <Pressable style={styles.item} onPress={() => jogar(3)}>
-                <Image style={styles.itemImagem} source={figura[3].imagem} />
+                <Image style={styles.itemImagem} source={figuras[3].imagem} />
               </Pressable>
               <Pressable style={styles.item} onPress={() => jogar(4)}>
-                <Image style={styles.itemImagem} source={figura[4].imagem} />
+                <Image style={styles.itemImagem} source={figuras[4].imagem} />
               </Pressable>
               <Pressable style={styles.item} onPress={() => jogar(5)}>
-                <Image style={styles.itemImagem} source={figura[5].imagem} />
+                <Image style={styles.itemImagem} source={figuras[5].imagem} />
               </Pressable>
             </View>
             <View style={styles.fileira}>
               <Pressable style={styles.item} onPress={() => jogar(6)}>
-                <Image style={styles.itemImagem} source={figura[6].imagem} />
+                <Image style={styles.itemImagem} source={figuras[6].imagem} />
               </Pressable>
               <Pressable style={styles.item} onPress={() => jogar(7)}>
-                <Image style={styles.itemImagem} source={figura[7].imagem} />
+                <Image style={styles.itemImagem} source={figuras[7].imagem} />
               </Pressable>
               <Pressable style={styles.item} onPress={() => jogar(8)}>
-                <Image style={styles.itemImagem} source={figura[8].imagem} />
+                <Image style={styles.itemImagem} source={figuras[8].imagem} />
               </Pressable>
             </View>
           </ScrollView>
